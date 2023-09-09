db685dcee4f331ee68b5fe6ffc161c5ac306b473
ForkEyeee <shawncarter123456@gmail.com>
2023-09-09 00:43:07 -0400

    set up fetching data from REST API for homepage

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import { Box, Heading } from "@chakra-ui/react";
 
 const Home = () => {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:5173/api/posts");
-        console.log(response);
+        // console.log(response);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
 
         const text = await response.text();
-        console.log(text);
+        // console.log(text);
         const data = JSON.parse(text);
         setMessage(data.message);
       } catch (error) {
@@ -23,10 +23,18 @@ const Home = () => {
     };
     fetchData();
   }, []);
-
+  console.log(message);
   return (
     <Box className="App">
-      <Heading>{message}</Heading>
+      {message.map(message => (
+        <Box>
+          <Box>{message._id}</Box>
+          <Box>{message.title}</Box>
+          <Box>{message.comments}</Box>
+          <Box>{message.time}</Box>
+          <Box>{message.published}</Box>
+        </Box>
+      ))}{" "}
     </Box>
   );
 };
